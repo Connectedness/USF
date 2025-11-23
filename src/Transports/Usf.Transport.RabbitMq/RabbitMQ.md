a3399d31e572128e345f3840a3ad269448e89750
Christian Flessa <chris@z0rn.net>
2025-11-23 16:41:34 +0100

    feat: wip RabbitMQ transport
    Signed-off-by: Christian Flessa <chris@z0rn.net>

--- src/Transports/Usf.Transport.RabbitMq/RabbitMQ.md
+++ src/Transports/Usf.Transport.RabbitMq/RabbitMQ.md
@@ -3,7 +3,7 @@
 * `ConnectionFactory`: used to create a long-lived `IConnection` instance using a URI (e.g.
   `amqp://guest:guest@localhost:5672`)
 * `IConnection`: represents a connection to the RabbitMQ server, long-lived, re-use
-* `IChannel`: represents a channel to the RabbitMQ server for most of the operations, short-lived, re-use´, create on
+* `IChannel`: represents a channel to the RabbitMQ server for most of the operations, short-lived, re-use, create on
   demand
 * `IAsyncBasicConsumer`: represents a consumer of messages from the RabbitMQ server. Can use `AsyncDefaultBasicConsumer`
   as a base class or `AsyncEventingBasicConsumer` as an entry without writing dedicated consumers
@@ -21,7 +21,8 @@ var factory = new ConnectionFactory
 };
 ```
 
-See [RabbitMQ.Client documentation](https://www.rabbitmq.com/api-guide.html#connection-recovery) for more information.
+See [RabbitMQ.Client documentation](https://www.rabbitmq.com/client-libraries/dotnet-api-guide#recovery) for more
+information.
 
 ## What We Need
 
@@ -34,14 +35,14 @@ Generally:
 Publishing messages to RabbitMQ:
 
 * A custom publisher service containing an `IChannel` for publishing to exchanges/queues
-* Use a middleware pipeline to transform/enrich messages before publishing? ❓
+* Use a middleware pipeline to transform/enrich messages before publishing?
 * The publisher should always use `publisher confirms` to ensure messages are delivered;
   see [Reliable Publishing with Publisher Confirms](https://www.rabbitmq.com/tutorials/tutorial-seven-dotnet)
 
 Receiving messages from RabbitMQ:
 
 * Use a (custom) consumer to listen for messages with the `IChannel`
-* Use a middleware pipeline to transform/enrich messages before processing? ❓
+* Use a middleware pipeline to transform/enrich messages before processing?
 * The consumer must preprocess the message to determine its type and route it to the appropriate handler
 * For each message a DI scope is created and the handler is resolved from the scope
 * The handler is called to process the message
---
+++ src/Transports/Usf.Transport.RabbitMq/RabbitStuff.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
+
+namespace Usf.Transport.RabbitMq;
+
+public class MessageReceiver : AsyncDefaultBasicConsumer
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public MessageReceiver(IChannel channel, IServiceProvider serviceProvider) : base(channel)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public override async Task HandleBasicDeliverAsync(
+        string consumerTag,
+        ulong deliveryTag,
+        bool redelivered,
+        string exchange,
+        string routingKey,
+        IReadOnlyBasicProperties properties,
+        ReadOnlyMemory<byte> body,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var context = new MessageContext
+        {
+            DeliveryTag = deliveryTag,
+            ConsumerTag = consumerTag,
+            Exchange = exchange,
+            RoutingKey = routingKey,
+            IsRedelivered = redelivered,
+            Properties = properties,
+            Body = body
+        };
+
+        await using var scope = _serviceProvider.CreateAsyncScope();
+
+        var consumerProvider = scope.ServiceProvider.GetRequiredService<IConsumerProvider>();
+        if (!consumerProvider.TryGetConsumerTypeForMessage(context, out var consumeType))
+        {
+            await Channel.BasicRejectAsync(deliveryTag, false, cancellationToken);
+            return;
+        }
+
+        var consumer = (IMessageConsumer) scope.ServiceProvider.GetRequiredService(consumeType);
+
+        await consumer.ConsumeMessageAsync(context, cancellationToken);
+
+
+        // var deserializer = scope.ServiceProvider.GetRequiredService<IMessageDeserializer>();
+        // var message = deserializer.DeserializeMessage<IMessage>(body.Span);
+    }
+}
+
+public record MessageContext
+{
+    public required ulong DeliveryTag { get; init; }
+    public required string ConsumerTag { get; init; }
+    public required string Exchange { get; init; }
+    public required string RoutingKey { get; init; }
+    public bool IsRedelivered { get; init; }
+    public required IReadOnlyBasicProperties Properties { get; init; }
+    public required ReadOnlyMemory<byte> Body { get; init; }
+}
+
+public interface IMessageConsumer
+{
+    Task ConsumeMessageAsync(MessageContext messageContext, CancellationToken cancellationToken = default);
+}
+
+public interface IConsumerProvider
+{
+    bool TryGetConsumerTypeForMessage(MessageContext messageContext, out Type consumerType);
+}
+
+public interface IMessageDeserializer
+{
+    TMessage DeserializeMessage<TMessage>(ReadOnlySpan<byte> message, Dictionary<string, object?>? headers = null);
+}
+
+public interface IMessageSerializer
+{
+    byte[] SerializeMessage<TMessage>(TMessage message);
+}
+
+public interface IMessagePublisher { }
