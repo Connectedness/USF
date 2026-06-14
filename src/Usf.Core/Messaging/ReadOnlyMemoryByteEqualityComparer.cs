7d33880880dc280eedb98e242d218f39c4cba893
Kenny Pflug <kenny.pflug@live.de>
2026-06-14 05:59:05 +0200

    perf: ReadOnlyMemoryByteEqualityComparer.GetHashCode now only uses length and the last byte for non-empty memory instances
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- src/Usf.Core/Messaging/ReadOnlyMemoryByteEqualityComparer.cs
+++ src/Usf.Core/Messaging/ReadOnlyMemoryByteEqualityComparer.cs
@@ -16,25 +16,9 @@ public sealed class ReadOnlyMemoryByteEqualityComparer : IEqualityComparer<ReadO
 
     public int GetHashCode(ReadOnlyMemory<byte> value)
     {
-        // We calculate the hash code with only the first 16 bytes and the last byte for memory instances
-        // that are longer than 16 bytes. This lets us achieve O(1) performance instead of
-        // O(n) for longer spans. Adding the length at the beginning is a reasonable way to avoid collisions.
+        // We calculate the hash code with only the length and the last byte. This lets us achieve O(1) performance
+        // instead of O(n) for longer spans.
         var span = value.Span;
-        HashCode hashCode = new ();
-        hashCode.Add(span.Length);
-
-        var prefixLength = Math.Min(span.Length, 16);
-
-        for (var index = 0; index < prefixLength; index++)
-        {
-            hashCode.Add(span[index]);
-        }
-
-        if (span.Length > prefixLength)
-        {
-            hashCode.Add(span[^1]);
-        }
-
-        return hashCode.ToHashCode();
+        return span.IsEmpty ? 0 : HashCode.Combine(span.Length, span[^1]);
     }
 }
